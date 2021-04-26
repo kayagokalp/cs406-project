1179cd07ef710e1f7f238f7fadbed57837ec7dca
Kaya Gökalp <kayagokalp@kaya-mbp.local>
2021-04-26 21:58:14 +0300

    file reader self cycle ignoring added

--- misc/build.sh
+++ misc/build.sh
@@ -1,2 +1,2 @@
 parentdir="$(dirname "$(pwd)")"
-g++ $parentdir/cs406-project/src/main.cpp -o $parentdir/cs406-project/build/program.out
+g++ $parentdir/cs406-project/src/main.cpp -o $parentdir/cs406-project/build/program.out -std=c++11
--- src/main.cpp
+++ src/main.cpp
@@ -9,7 +9,7 @@
 //These are global for now. This will change when the implementation finishes.
 std::vector<std::vector<int> > matrixOfEdges;
 std::vector<std::pair<int,int> > *edges;
-
+int numberOfVertices;
 
 void check_for_duplicates()
 {
@@ -17,7 +17,10 @@ void check_for_duplicates()
 	//Some of them does not have duplicates. To overcome this duality
 	//We are assuming there are no repeating (only a-b is available in the input)
 	//So we create b-a and then we check if there are any duplicates in the list.
+
+	//TODO(kaya): a-a edges should be deleted.
  
+	edges->erase(std::remove_if(edges->begin(),edges->end(), [] (std::pair<int,int> &p) {return p.first == p.second;}),edges->end());
 	std::sort(edges->begin(), edges->end());
 	edges->erase(std::unique(edges->begin(), edges->end()), edges->end());
 	#if DEBUG
@@ -44,6 +47,11 @@ bool read_file(std::string &filename)
 	edges = new std::vector<std::pair<int,int> >();
 	printf("filename : %s \n",filename.c_str());
 	std::ifstream inputFile(filename.c_str());
+	if(inputFile.fail())
+	{
+		std::cout<<"File does not exists\n";
+		return false;
+	}
 	std::string line;
 	while(getline(inputFile, line))
 	{
@@ -55,29 +63,49 @@ bool read_file(std::string &filename)
 		while(lineStream >> word)
 		{
 			if(count == 0)
+			{
 				index1 = stoi(word);
+			}
 			else if(count == 1)
+			{
 				index2 = stoi(word);
+			}
 			count++;
+			numberOfVertices = (index1 > numberOfVertices) ? index1 : numberOfVertices;
+			numberOfVertices = (index2 > numberOfVertices) ? index2 : numberOfVertices;
 		}
 		std::pair<int,int> pair(index1,index2);
 		std::pair<int,int> pair2(index2,index1);
 		edges->push_back(pair);
 		edges->push_back(pair2);
 	}
+	if(numberOfVertices != 0)
+	{
+		numberOfVertices++;
+	}
 	inputFile.close();
 	return true;	
 }
 
+void clear_used_mem()
+{
+	delete edges;
+}
 
 int main(int argc, char* argv[])
 {
+	numberOfVertices = 0;
 	std::string filename = "";
+	edges = NULL;
 	if(argc > 1)
 	{
 		filename = std::string(argv[1]);
 	}
-	read_file(filename);
-	check_for_duplicates();
+	if(read_file(filename))
+	{
+		std::cout<<" numberOfVertices = " << numberOfVertices << "\n";
+		check_for_duplicates();
+	}
+	clear_used_mem();
 	return 0;
 } 
